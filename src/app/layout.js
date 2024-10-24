06c2e1e34206dd4a4f4131e04e5368c6fe51d897
satasiyarahul <leonardo@quajato.com>
2024-10-24 20:33:36 +0530

    production fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Footer from "@/component/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Cookie from "@/component/Cookie";
 import TopButton from "@/component/TopButton";
-import CommingSoon from "@/component/CommingSoon";
+// import CommingSoon from "@/component/CommingSoon";
 
 export const metadata = {
   title: "REIG",
@@ -27,13 +27,13 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/svg" href="../public/img/REG.svg" />
       </head>
       <body>
-        {/* <Header />
+        <Header />
         <main>{children}</main>
         <SpeedInsights />
         <Cookie />
         <TopButton />
-        <Footer /> */}
-        <CommingSoon/>
+        <Footer />
+        {/* <CommingSoon/> */}
       </body>
     </html>
   );
